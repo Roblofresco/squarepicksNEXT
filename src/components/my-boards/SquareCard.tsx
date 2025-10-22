eaa0021bc7581feb956846eca7a5a753432a460e
Rob Sweet <you@example.com>
2025-10-22 11:48:59 -0400

    Add red REFUNDED badge for unfilled boards in History tab

--- src/components/my-boards/SquareCard.tsx
+++ src/components/my-boards/SquareCard.tsx
@@ -30,6 +30,8 @@ const getStatusAppearance = (status: BoardStatus | string, isLive?: boolean, isB
   }
   
   switch (status) {
+    case 'unfilled':
+      return { text: 'Refunded', color: 'bg-red-600 hover:bg-red-700', icon: <XCircle className="h-3.5 w-3.5" /> };
     case 'IN_PROGRESS_Q1':
     case 'IN_PROGRESS_Q2':
     case 'IN_PROGRESS_Q3':
